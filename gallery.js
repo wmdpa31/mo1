e4ee4762f89344f0e643aefb811b918f35263f52
nate <nate@nateui-MacBookAir.local>
2024-04-22 22:17:52 +0900

    사진첩 애니메이션 높이 이슈로 원복 & padding 수정

--- gallery.js
+++ gallery.js
@@ -1,9 +1,12 @@
 window.onload = () => {
   const showMoreBtn = document.getElementById('showMoreBtn');
   const collapseBtn = document.getElementById('collapseBtn');
-  const galleryImagesContainer = document.getElementById('galleryImages');
+  const hiddenImages = document.querySelectorAll('#gallery img.hidden');
 
   showMoreBtn.addEventListener('click', () => {
+    hiddenImages.forEach((img) => {
+      img.classList.remove('hidden');
+    });
     collapseBtn.style.display = 'inline-block';
     collapseBtn.style.backgroundColor = 'ffe08c';
     collapseBtn.style.width = '70px';
@@ -13,11 +16,15 @@ window.onload = () => {
     collapseBtn.style.borderRadius = '15px';
 
     showMoreBtn.style.display = 'none';
-    galleryImagesContainer.style.height = '550px';
   });
 
   collapseBtn.addEventListener('click', () => {
+    const galleryImages = document.querySelectorAll('#addGalleyItem');
+    galleryImages.forEach((img) => {
+      img.classList.add('hidden');
+    });
     collapseBtn.style.display = 'none';
+
     showMoreBtn.style.display = 'inline-block';
     showMoreBtn.style.backgroundColor = 'ffe08c';
     showMoreBtn.style.width = '70px';
@@ -25,8 +32,6 @@ window.onload = () => {
     showMoreBtn.style.display = 'block';
     showMoreBtn.style.margin = '7% auto';
     showMoreBtn.style.borderRadius = '15px';
-
-    galleryImagesContainer.style.height = '350px';
   });
 
   const galleryImages = document.querySelectorAll('.gallery img');
